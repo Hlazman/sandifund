f8b966bacfbff32e7ddac8973d55ecf8b298e0b6
ITSurgeon <stookal@gmail.com>
2025-09-13 09:08:05 +0000

    feat: add sticker and translation content types

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+export default () => ({
+    graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      landingPage: true,
+      depthLimit: 10,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+});
