fad3b5979e2b262db07c41609814e89c9a522abf
Hlazman <glazman.b@gmail.com>
2025-10-11 19:11:21 +0300

    made Booked page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,3 @@
-// import React from "react";
-// import { createRoot } from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
-// import { ApolloProvider } from "@apollo/client/react"; // ← ВАЖНО: из субпакета react
-// import { LanguageProvider } from "./context/LanguageContext";
-// import client from "./api/apolloClient";
-// import App from "./App";
-// import "./index.css";
-
-// const root = createRoot(document.getElementById("root"));
-// root.render(
-//   <ApolloProvider client={client}>
-//     <BrowserRouter>
-//       {/* <App /> */}
-//       <LanguageProvider>
-//         <App />
-//       </LanguageProvider>
-//     </BrowserRouter>
-//   </ApolloProvider>
-// );
-
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
