0bec1292fdbfa8ce6b3af49ae0304afd987d4bef
Hlazman <glazman.b@gmail.com>
2025-12-19 17:14:48 +0200

    add TODO comment

--- mobile/src/components/cards/CardProduct.js
+++ mobile/src/components/cards/CardProduct.js
@@ -442,6 +442,7 @@ export default function CardProduct(props) {
                   backgroundColor: pressed ? "#f3f4f6" : "transparent",
                 })}
               >
+                {/* TODO */}
                 <Text style={{ fontSize: 16 }}>Other contact</Text>
               </Pressable>
             ) : null}
